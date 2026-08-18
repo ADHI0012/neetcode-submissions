d16035cca56ac539cf3b74698ac856d7b8912c90
ADHITHYA <adhia1319@gmail.com>
2026-08-18 15:25:36 +0530

    Add: number-of-provinces - submission-0

---
+++ Data Structures & Algorithms/number-of-provinces/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        cities = len(isConnected)
+        visited = set()
+        provinces = 0
+        q = collections.deque()
+
+
+        def bfs(i):
+            q.append(i)
+            visited.add(i)
+            while q:
+                x = q.popleft()
+                for j in range(len(isConnected[x])):
+                    if isConnected[x][j] and j not in visited:
+                        visited.add(j)
+                        q.append(j)
+
+
+
+
+        for i in range(cities):
+            for j in range(cities):
+                if isConnected[i][j] and i not in visited:
+                    bfs(i)
+                    provinces += 1
+
+
+        return provinces
